d7f76f98ab1366f09ce5c87d2a5466c49950c9ba
dengqiming <dengqiming@xiaomadada.com>
2016-01-24 20:08:19 +0800

    元宵活动页面

---
+++ app/scripts/prize.js
@@ -0,0 +1,64 @@
+'use strict';
+
+$(function() {
+  FastClick.attach(document.body);
+});
+
+var modalTemplate = '' +
+  '<section class="modal modal-alert">' +
+    '<div class="overlay"></div>' +
+    '<div class="modal-table">' +
+      '<div class="modal-table-cell">' +
+        '<div class="modal-panel">' +
+          '<div class="modal-header">' +
+            '<div class="modal-close"></div>' +
+          '</div>' +
+          '<div class="modal-content">' +
+            '<div class="modal-text">' +
+              '<div class="alert-text">' +
+                '<div class="text"></div>' +
+              '</div>' +
+              '<div class="btn-panel">' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+    '</div>' +
+  '</section>';
+
+
+$('.btn-get').click(function(){
+  // 领取
+  var modalAlert = $(modalTemplate).css('display', 'block');
+  modalAlert.find('.text')
+    .append('成功领取！');
+  modalAlert.find('.btn-panel')
+    .append('<a href="index.html" class="btn btn-default btn-beta active btn-again">去抽奖</a>')
+    .append('<button type="button" class="btn btn-default btn-beta">去使用</button>');
+  modalAlert.appendTo('body');
+  modalAlert.find('.modal-close').click(function(){
+    modalAlert.remove();
+  });
+  modalAlert.find('.btn-again').click(function(){
+    modalAlert.remove();
+  });
+});
+
+if(false) {
+  // 领取
+  var modalAlert = $(modalTemplate).css('display', 'block');
+  modalAlert.find('.text')
+    .append('没有奖品可领取！');
+  modalAlert.find('.btn-panel')
+    .append('<a href="index.html" class="btn btn-default btn-beta active btn-again">去抽奖</a>');
+  modalAlert.appendTo('body');
+  modalAlert.find('.modal-close').click(function(){
+    modalAlert.remove();
+  });
+  modalAlert.find('.btn-again').click(function(){
+    modalAlert.remove();
+  });
+};
+
+console.log('Hello world');
